2fcb23a89d11ce9cde778ed5b7ba52d033b7bb39
yuvrajmodz <ys8619050@gmail.com>
2025-10-17 22:59:50 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,32 @@
+# NitroExpose
+
+**Letest Version:** 1.3  
+**Developer:** @Nactire  
+**Git Repo:** [GitHub](https://github.com/yuvrajmodz/NitroExpose)
+
+• Easily Expose Your Local Port to Your Domain.  
+• Automatic SSL installation.  
+• Automatic Tests To Your Site.  
+• Expose Within 4 Seconds.  
+• Our Software Needs Root Environment.
+
+## Installation
+
+```bash
+pip install NitroExpose --break-system-packages
+```
+
+## Usage
+
+```bash
+NitroExpose
+```
+
+## Optional installation
+
+```bash
+sudo apt update -y
+sudo apt install nginx -y
+sudo apt install certbot -y
+sudo apt install python3-certbot-nginx -y
+```
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="NitroExpose",
+    version="1.3",
+    author="@NacDevs",
+    author_email="yuvrajmodz@gmail.com",
+    description="Advanced CLI To Expose Port To Your Domain.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yuvrajmodz/NitroExpose",
+    packages=find_packages(),
+    python_requires='>=3.8',
+    install_requires=[
+        "requests"
+    ],
+    entry_points={
+        'console_scripts': [
+            'NitroExpose=nitroexpose.cli:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
+)
